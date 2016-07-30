fe4ae0ec0df6ed4c028d899bf18584dac7a928c7
Sterv <stephensulliv@gmail.com>
2016-07-30 15:47:54 -0400

    add cardReader module

--- src/hardwareModules/cardReader.js
+++ src/hardwareModules/cardReader.js
@@ -1,6 +1,26 @@
+var onData = null
+
 function onInput(callback) {
-  setTimeout(() => callback(':00050?'), 2000)
+  // setTimeout(() => callback(':00050?'), 2000)
+  onData = callback;
 }
+
+var stdin = process.openStdin(); 
+// require('tty').setRawMode(true);    
+
+var userID = '';
+var flag = true;
+
+stdin.on('keypress', function (chunk, key) {
+  // console.log('Get Chunk: ' + chunk + '\n');
+  userID += chunk;
+  
+  if (key && key.ctrl && key.name == 'c') process.exit();
+  if (key.name === 'return') {
+    onData(userID)
+    console.log('userID',userID);
+  }
+});
 
 module.exports = {
   onInput: onInput
